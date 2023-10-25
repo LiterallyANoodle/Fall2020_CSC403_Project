7226987a0948f5acb83b91c5a524e8021005ac5f
UjjenRaj <ujjenrajkarnikar@gmail.com>
2023-10-25 14:29:00 -0500

    changed start screen to add map

--- CSC 403 Game/Menu2.gd
+++ CSC 403 Game/Menu2.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene("res://map_1.tscn")
+	get_tree().change_scene_to_file("res://map-system.tscn")
 
 
 func _on_options_pressed():
