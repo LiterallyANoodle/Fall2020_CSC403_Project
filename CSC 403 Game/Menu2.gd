085a4d5c33de92680041a025fa4712641b883ec9
UjjenRaj <ujjenrajkarnikar@gmail.com>
2023-10-25 13:57:30 -0500

    added 2 menu files, menu2 is the working file

---
+++ CSC 403 Game/Menu2.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/Label/VBoxContainer/Start.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene("res://map_1.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
