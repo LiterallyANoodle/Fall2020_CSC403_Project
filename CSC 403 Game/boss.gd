ecde772b97bbad02555938fbeb01ae15f772c931
ZachNalepa <zachnalepa@gmail.com>
2023-11-10 15:56:40 -0600

    added boss and enemy guns, enemy guns point at player, boss guns rotate around boss

--- CSC 403 Game/boss.gd
+++ CSC 403 Game/boss.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 #class_name Enemy
 
+var gun = preload("res://BossGun.tscn").instantiate()
+var gun2 = preload("res://BossGun.tscn").instantiate()
+
+var gun_array = Array()
+
 @export var speed = 400
 var screen_size
 var enemy_id
@@ -10,6 +15,11 @@ var health
 func _ready():
 	screen_size = get_viewport_rect().size
 	self.health = 20
+	self.add_child(gun)
+	self.add_child(gun2)
+	gun_array.append(gun)
+	gun_array.append(gun2)
+	gun2.set_rot(120)
 	
 	
 func _physics_process(delta):
@@ -19,7 +29,6 @@ func _physics_process(delta):
 func _process(delta):
 	var velocity = Vector2.ZERO
 	self.position = position.clamp(Vector2.ZERO, screen_size)
-
 
 func start(pos, given_id):
 	self.position = pos
@@ -33,3 +42,8 @@ func _on_enemy_hitbox_body_entered(body):
 	if body.has_method("PlayerBullet"):
 		health -= 2
 		print("player attacked")
+
+func _on_timer_timeout():
+	for item in gun_array:
+		item.shoot_left(self.get_node("."))
+	pass # Replace with function body.
