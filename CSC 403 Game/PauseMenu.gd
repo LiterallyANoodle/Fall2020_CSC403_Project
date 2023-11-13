43e9f66ed608454fbebe5b05b776d75cdab5f543
UjjenRaj <ujjenrajkarnikar@gmail.com>
2023-11-12 20:34:51 -0600

    added comments to the source files

--- CSC 403 Game/PauseMenu.gd
+++ CSC 403 Game/PauseMenu.gd
@@ -1,33 +1,36 @@
 extends Control
 
-#@export var game_manager : FinalGame
-@export var game_manager : NewGame 
-# Called when the node enters the scene tree for the first time
-
+## Reference to the game manager
+@export var game_manager : NewGame
+ 
+## Called when the node enters the scene tree for the first time
 func _ready():
 	hide()
 	game_manager.connect("toggle_game_paused", _on_final_game_toggle_game_paused)
 	$CenterContainer/VBoxContainer/Resume.grab_focus()
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+## Handler for the game manager's toggle_game_paused signal 
 func _on_final_game_toggle_game_paused(is_paused : bool):
 	if(is_paused):
 		show()
 	else:
 		hide()
 
+## Handler for the resume button pressed
 func _on_resume_pressed():
 	game_manager.game_paused = false
 	$CenterContainer/VBoxContainer/Resume.grab_focus()
 
-
+## Handler for the main menu button pressed
 func _on_quit_pressed():
 	get_tree().quit()
 
+## Handler for the main menu button pressed
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://menu_2.tscn")
 	self.queue_free()
--- CSC 403 Game/newgame.gd
+++ CSC 403 Game/newgame.gd
@@ -1,19 +1,28 @@
 extends Node
 
+## Custon class for the main game, named NewGame
 class_name NewGame
 
+# Signal emitted when the game is toggled between paused and unpaused states
 signal toggle_game_paused(is_paused : bool)
+
+# Reference to the map system scene, of type PackedScene
 var map = preload("res://map-system.tscn")
+
+# Reference to the map system scene, of type PackedScene
 var spawn
 
+# Variable to track the game paused state
 var game_paused : bool = false:
 	get:
 		return game_paused
 	set(value):
+		# Set the game paused state, pause/unpause the tree, and emit the signal
 		game_paused = value
 		get_tree().paused = !game_paused
 		emit_signal("toggle_game_paused", game_paused)
 
+# Input handling for the game, toggles pause on 'ui_cancel' input
 func _input(event : InputEvent):
 	if(event.is_action_pressed("ui_cancel")):
 		game_paused = !game_paused
@@ -21,5 +30,6 @@ func _input(event : InputEvent):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Instantiate the map system and add it as a child of the main game
 	spawn = map.instantiate()
 	add_child(spawn)
